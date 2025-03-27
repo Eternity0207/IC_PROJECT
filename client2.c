e9ba3af5e460b8ca811ec4d23919dcfa0c3c083d
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-27 17:32:22 +0530

    Updated vlients to use server_ip

--- client2.c
+++ client2.c
@@ -47,7 +47,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     struct sockaddr_in serv = {0};
     serv.sin_family = AF_INET;
     serv.sin_port = htons(PORT);
-    serv.sin_addr.s_addr = inet_addr("10.24.2.227");
+    serv.sin_addr.s_addr = inet_addr("SERVER_IP");
     
     if (connect(client_socket, (struct sockaddr *)&serv, sizeof(serv)) == SOCKET_ERROR) {
         MessageBox(NULL, "Failed to connect to server!", "Error", MB_OK);
