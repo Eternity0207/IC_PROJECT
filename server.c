4af24ed38241f505872ef80b171086c52efbc2c6
HARSHVARANDANI <hpvarandani@gmail.com>
2025-04-11 00:09:06 +0530

    Updated server for IP based connection

--- server.c
+++ server.c
@@ -1,134 +1,239 @@
-#define _WIN32_WINNT 0x0600 // Ensure compatibility with Vista or later
-
+#define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 4096
+#define MAX_CLIENTS 100
 
-int listener = 0;
-struct sockaddr_in serv;
-int maxFd = 0;
-int client[2] = {0, 0};
-fd_set fr;
+typedef struct ClientNode {
+    SOCKET socket;
+    char ip[INET6_ADDRSTRLEN];
+    SOCKET paired_socket;
+    struct ClientNode *next;
+} ClientNode;
 
-void acceptConnection() {
-    int clientSocket = accept(listener, NULL, NULL);
-    if (clientSocket < 0) {
-        printf("Error accepting connection! (Error Code: %d)\n", WSAGetLastError());
-        return;
-    }
+ClientNode *client_list = NULL;
+SOCKET listener = INVALID_SOCKET;
+fd_set master_fd_set;
 
-    for (int index = 0; index < 2; index++) {
-        if (client[index] == 0) {
-            client[index] = clientSocket;
-            printf("Client %d connected, socket: %d\n", index + 1, clientSocket);
-
-            if (client[index] > maxFd) {
-                maxFd = client[index] + 1;
-            }
-            return;
-        }
-    }
-
-    printf("Server is full! Rejecting connection...\n");
-    closesocket(clientSocket);
+void add_client(SOCKET socket, const char *ip) {
+    ClientNode *new_node = (ClientNode *)malloc(sizeof(ClientNode));
+    new_node->socket = socket;
+    strncpy(new_node->ip, ip, INET6_ADDRSTRLEN);
+    new_node->paired_socket = INVALID_SOCKET;
+    new_node->next = client_list;
+    client_list = new_node;
+    printf("New client connected: %s\n", ip);
 }
 
-void receiveClientMessage(int senderIndex) {
-    int senderSocket = client[senderIndex];
-    int receiverIndex = (senderIndex == 0) ? 1 : 0;
-    int receiverSocket = client[receiverIndex];
-
-    char msg[256] = {0};
-    int err = recv(senderSocket, msg, sizeof(msg) - 1, 0);
-
-    if (err <= 0) {
-        printf("Client %d disconnected\n", senderIndex + 1);
-        closesocket(client[senderIndex]);
-        FD_CLR(client[senderIndex], &fr);
-        client[senderIndex] = 0;
-
-        maxFd = listener;
-        for (int i = 0; i < 2; i++) {
-            if (client[i] > maxFd) {
-                maxFd = client[i];
+void remove_client(SOCKET socket) {
+    ClientNode *prev = NULL;
+    ClientNode *current = client_list;
+    
+    while (current != NULL) {
+        if (current->socket == socket) {
+            // If this client was paired, notify the other client
+            if (current->paired_socket != INVALID_SOCKET) {
+                send(current->paired_socket, "PARTNER_DISCONNECTED", 20, 0);
+                
+                // Find and update the paired client
+                ClientNode *temp = client_list;
+                while (temp != NULL) {
+                    if (temp->socket == current->paired_socket) {
+                        temp->paired_socket = INVALID_SOCKET;
+                        break;
+                    }
+                    temp = temp->next;
+                }
             }
+            
+            // Remove from list
+            if (prev == NULL) {
+                client_list = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            
+            free(current);
+            printf("Client removed\n");
+            return;
         }
-        maxFd += 1;
-    } else {
-        msg[err] = '\0';
-        printf("Message from Client %d: %s\n", senderIndex + 1, msg);
+        prev = current;
+        current = current->next;
+    }
+}
 
-        if (receiverSocket != 0) {
-            send(receiverSocket, msg, strlen(msg), 0);
-        } else {
-            char *waitingMsg = "[SERVER] Waiting for another client to connect...";
-            send(senderSocket, waitingMsg, strlen(waitingMsg), 0);
+ClientNode *find_client_by_ip(const char *ip) {
+    ClientNode *current = client_list;
+    while (current != NULL) {
+        if (strcmp(current->ip, ip) == 0) {
+            return current;
         }
+        current = current->next;
+    }
+    return NULL;
+}
+
+ClientNode *find_client_by_socket(SOCKET socket) {
+    ClientNode *current = client_list;
+    while (current != NULL) {
+        if (current->socket == socket) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void handle_ip_request(SOCKET sender_socket, const char *requested_ip) {
+    ClientNode *sender = find_client_by_socket(sender_socket);
+    if (sender == NULL) return;
+    
+    ClientNode *target = find_client_by_ip(requested_ip);
+    
+    if (target != NULL && target != sender && target->paired_socket == INVALID_SOCKET) {
+        // Pair the clients
+        sender->paired_socket = target->socket;
+        target->paired_socket = sender->socket;
+        
+        send(sender_socket, "IP_FOUND", 8, 0);
+        send(target->socket, "IP_FOUND", 8, 0);
+        
+        printf("Clients paired: %s <-> %s\n", sender->ip, target->ip);
+    } else {
+        send(sender_socket, "IP_NOT_FOUND", 12, 0);
+    }
+}
+
+void forward_message(SOCKET sender_socket, const char *msg, int msg_len) {
+    ClientNode *sender = find_client_by_socket(sender_socket);
+    if (sender == NULL || sender->paired_socket == INVALID_SOCKET) return;
+    
+    send(sender->paired_socket, msg, msg_len, 0);
+}
+
+void accept_new_connection() {
+    struct sockaddr_in client_addr;
+    int addr_len = sizeof(client_addr);
+    SOCKET new_socket = accept(listener, (struct sockaddr *)&client_addr, &addr_len);
+    
+    if (new_socket == INVALID_SOCKET) {
+        printf("Accept failed with error: %d\n", WSAGetLastError());
+        return;
+    }
+    
+    // Get client IP
+    char client_ip[INET6_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+    
+    add_client(new_socket, client_ip);
+    FD_SET(new_socket, &master_fd_set);
+    
+    // Send welcome message with instructions
+    const char *welcome_msg = "Welcome! Send the IP address of the client you want to connect to in the format: IP_Address:xxx.xxx.xxx.xxx";
+    send(new_socket, welcome_msg, strlen(welcome_msg), 0);
+}
+
+void handle_client_message(SOCKET client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+    
+    if (bytes_received <= 0) {
+        // Client disconnected
+        closesocket(client_socket);
+        FD_CLR(client_socket, &master_fd_set);
+        remove_client(client_socket);
+        return;
+    }
+    
+    buffer[bytes_received] = '\0';
+    
+    ClientNode *client = find_client_by_socket(client_socket);
+    if (client == NULL) return;
+    
+    if (client->paired_socket == INVALID_SOCKET) {
+        // Handle IP address request
+        if (strncmp(buffer, "IP_Address:", 11) == 0) {
+            char requested_ip[INET6_ADDRSTRLEN];
+            strncpy(requested_ip, buffer + 11, INET6_ADDRSTRLEN);
+            handle_ip_request(client_socket, requested_ip);
+        }
+    } else {
+        // Forward message to paired client
+        forward_message(client_socket, buffer, bytes_received);
     }
 }
 
 int main(void) {
     WSADATA ws;
-    if (WSAStartup(MAKEWORD(2, 2), &ws) < 0) {
-        exit(EXIT_FAILURE);
+    if (WSAStartup(MAKEWORD(2, 2), &ws) != 0) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        return 1;
     }
-
+    
     listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listener < 0) {
-        exit(EXIT_FAILURE);
+    if (listener == INVALID_SOCKET) {
+        printf("Socket creation failed: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
     }
-
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons(PORT);
-    InetPtonA(AF_INET, "YOUR_IP", &serv.sin_addr); // Use InetPtonA for IP address conversion
-    memset(&serv.sin_zero, 0, sizeof(serv.sin_zero));
-
-    int err = bind(listener, (struct sockaddr *)&serv, sizeof(serv));
-    if (err < 0) {
-        exit(EXIT_FAILURE);
+    
+    // Set socket to reuse address
+    int reuse = 1;
+    if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(reuse)) < 0) {
+        printf("setsockopt failed: %d\n", WSAGetLastError());
     }
-
-    err = listen(listener, 2);
-    if (err < 0) {
-        exit(EXIT_FAILURE);
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(listener, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("Bind failed: %d\n", WSAGetLastError());
+        closesocket(listener);
+        WSACleanup();
+        return 1;
     }
-
-    maxFd = listener + 1;
-    struct timeval tv;
-    tv.tv_sec = 2;
-    tv.tv_usec = 0;
-
+    
+    if (listen(listener, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed: %d\n", WSAGetLastError());
+        closesocket(listener);
+        WSACleanup();
+        return 1;
+    }
+    
+    printf("Server started on port %d\n", PORT);
+    
+    FD_ZERO(&master_fd_set);
+    FD_SET(listener, &master_fd_set);
+    
     while (1) {
-        FD_ZERO(&fr);
-        FD_SET(listener, &fr);
-
-        for (int i = 0; i < 2; i++) {
-            if (client[i] != 0) {
-                FD_SET(client[i], &fr);
-                if (client[i] >= maxFd) {
-                    maxFd = client[i] + 1;
-                }
-            }
+        fd_set read_fds = master_fd_set;
+        int socket_count = select(0, &read_fds, NULL, NULL, NULL);
+        
+        if (socket_count <= 0) {
+            printf("Select error: %d\n", WSAGetLastError());
+            continue;
         }
-
-        err = select(maxFd, &fr, NULL, NULL, &tv);
-        if (err < 0) {
-            exit(EXIT_FAILURE);
-        } else if (err > 0) {
-            if (FD_ISSET(listener, &fr)) {
-                acceptConnection();
-            } else {
-                for (int i = 0; i < 2; i++) {
-                    if (FD_ISSET(client[i], &fr)) {
-                        receiveClientMessage(i);
-                    }
+        
+        for (SOCKET i = 0; i < FD_SETSIZE; i++) {
+            if (FD_ISSET(i, &read_fds)) {
+                if (i == listener) {
+                    accept_new_connection();
+                } else {
+                    handle_client_message(i);
                 }
             }
         }
     }
-
+    
+    // Cleanup (though we never get here in this simple server)
+    closesocket(listener);
+    WSACleanup();
     return 0;
 }
