f0b3857876f587be263d682fca5e316d16c4d823
pexx3r <katariapaaras@gmail.com>
2025-04-13 23:02:07 +0530

    Indentation Fixes

--- auth.c
+++ auth.c
@@ -8,17 +8,14 @@ int validateCredentials(const char *username, const char *password) {
     if (strlen(username) == 0 || strlen(username) > MAX_USERNAME_LEN - 1) {
         return 0;
     }
-    
     if (strlen(password) == 0 || strlen(password) > MAX_PASSWORD_LEN - 1) {
         return 0;
     }
-    
     for (int i = 0; i < strlen(username); i++) {
         if (username[i] == ':' || username[i] == '\n') {
             return 0;
         }
     }
-    
     return 1;
 }
 
@@ -27,7 +24,6 @@ int userExists(const char *username) {
     if (!file) {
         return 0;
     }
-    
     UserCredentials user;
     while (fread(&user, sizeof(UserCredentials), 1, file)) {
         if (strcmp(user.username, username) == 0) {
@@ -35,7 +31,6 @@ int userExists(const char *username) {
             return 1;
         }
     }
-    
     fclose(file);
     return 0;
 }
@@ -44,21 +39,17 @@ int authenticateUser(const char *username, const char *password) {
     if (!validateCredentials(username, password)) {
         return 0;
     }
-    
     FILE *file = fopen(USERS_FILE, "rb");
     if (!file) {
         return 0;
-    }
-    
+    }    
     UserCredentials user;
-    
     while (fread(&user, sizeof(UserCredentials), 1, file)) {
         if (strcmp(user.username, username) == 0) {
             fclose(file);
             return (strcmp(user.password, password) == 0);
         }
     }
-    
     fclose(file);
     return 0;
 }
@@ -67,20 +58,16 @@ int registerUser(const char *username, const char *password) {
     if (!validateCredentials(username, password)) {
         return 0;
     }
-    
     if (userExists(username)) {
         return 0;
-    }
-    
+    }    
     FILE *file = fopen(USERS_FILE, "ab");
     if (!file) {
         return 0;
-    }
-    
+    }    
     UserCredentials newUser;
     strncpy(newUser.username, username, MAX_USERNAME_LEN);
     strncpy(newUser.password, password, MAX_PASSWORD_LEN);
-    
     int result = fwrite(&newUser, sizeof(UserCredentials), 1, file);
     fclose(file);
     
@@ -92,7 +79,6 @@ void initializeAuthSystem() {
     if (file) {
         fclose(file);
     }
-    
     file = fopen(USERS_FILE, "rb");
     if (file) {
         fseek(file, 0, SEEK_END);
